1ce520deb5abdcd2b7f4aaa1d56d564c9eefde0c
Debak Sen <52042726+debaksen@users.noreply.github.com>
2019-06-25 04:34:45 +0530

    Add files via upload

---
+++ Weather monitoring System using TCP/DHT_Mdns_advance.ino
@@ -0,0 +1,209 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiClient.h>
+#include <DHT.h>
+
+// Uncomment one of the lines below for whatever DHT sensor type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+
+#ifndef STASSID
+#define STASSID "tiuiotlab"
+#define STAPSK  "eng@tiu11"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+// TCP server at port 80 will respond to HTTP requests
+WiFiServer server(80);
+
+// DHT Sensor
+uint8_t DHTPin = D2; 
+               
+// Initialize DHT sensor.
+DHT dht(DHTPin, DHTTYPE);
+void setup(void) {
+  Serial.begin(9600);
+  delay(100);
+  
+  pinMode(DHTPin, INPUT);
+
+  dht.begin();
+  // Connect to WiFi network
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Set up mDNS responder:
+  // - first argument is the domain name, in this example
+  //   the fully-qualified domain name is "esp8266.local"
+  // - second argument is the IP address to advertise
+  //   we send our IP address on the WiFi network
+  if (!MDNS.begin("debaksen")) {
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+
+  // Start TCP (HTTP) server
+  server.begin();
+  Serial.println("TCP server started");
+
+  // Add service to MDNS-SD
+  MDNS.addService("http", "tcp", 80);
+}
+
+void loop(void) {
+
+  MDNS.update();
+
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  Serial.println("");
+  Serial.println("New client");
+
+  // Wait for data from client to become available
+  while (client.connected() && !client.available()) {
+    delay(1);
+  }
+
+  // Read the first line of HTTP request
+  String req = client.readStringUntil('\r');
+
+  // First line of HTTP request looks like "GET /path HTTP/1.1"
+  // Retrieve the "/path" part by finding the spaces
+  int addr_start = req.indexOf(' ');
+  int addr_end = req.indexOf(' ', addr_start + 1);
+  if (addr_start == -1 || addr_end == -1) {
+    Serial.print("Invalid request: ");
+    Serial.println(req);
+    return;
+  }
+  req = req.substring(addr_start + 1, addr_end);
+  Serial.print("Request: ");
+  Serial.println(req);
+  client.flush();
+
+  String s;
+  if (req == "/") {
+  // Read Humidity
+    float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+    float f = dht.readTemperature(true);
+  // check for sensor error
+      if (isnan(h) || isnan(t) || isnan(f)) {
+        Serial.println(F("Failed to read from DHT sensor!"));
+        return;
+        }
+   // Compute heat index in Fahreheit
+    float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+    float hic = dht.computeHeatIndex(t, h, false);
+    
+    IPAddress ip = WiFi.localIP();
+    String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+    s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+    s += SendHTML(hic,hif,h);
+    s +="\r\n\r\n";
+    
+    Serial.println("Sending 200");
+  } else {
+    s = "HTTP/1.1 404 Not Found\r\n\r\n";
+    Serial.println("Sending 404");
+  }
+  client.print(s);
+
+  Serial.println("Done with client");
+}
+
+String SendHTML(float TempCstat,float TempFstat,float Humiditystat){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<meta http-equiv=\"refresh\" content=\"2\">";
+  ptr +="<link href=\"https://fonts.googleapis.com/css?family=Open+Sans:300,400,600\" rel=\"stylesheet\">\n";
+  ptr +="<title>ESP8266 Weather Report</title>\n";
+  ptr +="<style>html { font-family: 'Open Sans', sans-serif; display: block; margin: 0px auto; text-align: center;color: #333333;}\n";
+  ptr +="body{margin-top: 50px;}\n";
+  ptr +="h1 {margin: 50px auto 30px;}\n";
+  ptr +=".side-by-side{display: inline-block;vertical-align: middle;position: relative;}\n";
+  ptr +=".humidity-icon{background-color: #3498db;width: 30px;height: 30px;border-radius: 50%;line-height: 36px;}\n";
+  ptr +=".humidity-text{font-weight: 600;padding-left: 15px;font-size: 19px;width: 160px;text-align: left;}\n";
+  ptr +=".humidity{font-weight: 300;font-size: 60px;color: #3498db;}\n";
+  ptr +=".temperature-icon{background-color: #f39c12;width: 30px;height: 30px;border-radius: 50%;line-height: 40px;}\n";
+  ptr +=".temperature-text{font-weight: 600;padding-left: 15px;font-size: 19px;width: 160px;text-align: left;}\n";
+  ptr +=".temperature{font-weight: 300;font-size: 60px;color: #f39c12;}\n";
+  ptr +=".superscript{font-size: 17px;font-weight: 600;position: absolute;right: -20px;top: 15px;}\n";
+  ptr +=".data{padding: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="<script>\n";
+  ptr +="setInterval(loadDoc,200);\n";
+  ptr +="function loadDoc() {\n";
+  ptr +="var xhttp = new XMLHttpRequest();\n";
+  ptr +="xhttp.onreadystatechange = function() {\n";
+  ptr +="if (this.readyState == 4 && this.status == 200) {\n";
+  ptr +="document.getElementById(\"webpage\").innerHTML =this.responseText}\n";
+  ptr +="};\n";
+  ptr +="xhttp.open(\"GET\", \"/\", true);\n";
+  ptr +="xhttp.send();\n";
+  ptr +="}\n";
+  ptr +="</script>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<div id=\"webpage\">\n";
+  ptr +="<h1>ESP8266 NodeMCU Weather Report</h1>\n";
+  ptr +="<div class=\"data\">\n";
+  ptr +="<div class=\"side-by-side temperature-icon\">\n";
+  ptr +="<svg version=\"1.1\" id=\"Layer_1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n";
+  ptr +="width=\"9.915px\" height=\"22px\" viewBox=\"0 0 9.915 22\" enable-background=\"new 0 0 9.915 22\" xml:space=\"preserve\">\n";
+  ptr +="<path fill=\"#FFFFFF\" d=\"M3.498,0.53c0.377-0.331,0.877-0.501,1.374-0.527C5.697-0.04,6.522,0.421,6.924,1.142\n";
+  ptr +="c0.237,0.399,0.315,0.871,0.311,1.33C7.229,5.856,7.245,9.24,7.227,12.625c1.019,0.539,1.855,1.424,2.301,2.491\n";
+  ptr +="c0.491,1.163,0.518,2.514,0.062,3.693c-0.414,1.102-1.24,2.038-2.276,2.594c-1.056,0.583-2.331,0.743-3.501,0.463\n";
+  ptr +="c-1.417-0.323-2.659-1.314-3.3-2.617C0.014,18.26-0.115,17.104,0.1,16.022c0.296-1.443,1.274-2.717,2.58-3.394\n";
+  ptr +="c0.013-3.44,0-6.881,0.007-10.322C2.674,1.634,2.974,0.955,3.498,0.53z\"/>\n";
+  ptr +="</svg>\n";
+  ptr +="</div>\n";
+
+  ptr +="<div class=\"side-by-side temperature-text\">Temperature</div>\n";
+  ptr +="<div class=\"side-by-side temperature\">";
+  ptr +=(int)TempCstat;
+  ptr +="<span class=\"superscript\">°C</span></div>\n";
+  ptr +="</div>\n";
+  ptr +="<p>Humidity: ";
+  ptr +="<div class=\"data\">\n";
+  ptr +="<div class=\"side-by-side humidity-icon\">\n";
+  ptr +="<svg version=\"1.1\" id=\"Layer_2\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\"\n\"; width=\"12px\" height=\"17.955px\" viewBox=\"0 0 13 17.955\" enable-background=\"new 0 0 13 17.955\" xml:space=\"preserve\">\n";
+  ptr +="<path fill=\"#FFFFFF\" d=\"M1.819,6.217C3.139,4.064,6.5,0,6.5,0s3.363,4.064,4.681,6.217c1.793,2.926,2.133,5.05,1.571,7.057\n";
+  ptr +="c-0.438,1.574-2.264,4.681-6.252,4.681c-3.988,0-5.813-3.107-6.252-4.681C-0.313,11.267,0.026,9.143,1.819,6.217\"></path>\n";
+  ptr +="</svg>\n";
+  ptr +="</div>\n";
+  ptr +="<div class=\"side-by-side humidity-text\">Humidity</div>\n";
+  ptr +="<div class=\"side-by-side humidity\">";
+  ptr +=(int)Humiditystat;
+  ptr +="<span class=\"superscript\">%</span></div>\n";
+  ptr +="</div>\n";
+
+  ptr +="</div>\n";
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+return ptr;
+}
---
+++ Weather monitoring System using TCP/README.md
@@ -0,0 +1,15 @@
+#TCP BASED WEATHER MONITORING SYSTEM USING DHT11 SENSOR USING MDNS
+
+
+### Introduction : 
+
+TCP is connection oriented (Transmission Control Protocol) i.e. TCP tracks all data sent, it requires acknowledgment for each process of the connection stages (octet).Because of this TCP is considered TCP is considered more reliable transfer protocol unlike UDP where it sends
+the information randomly without any acknowledgment from the other side. Here in this project we will build a weather monitoring system based upon TCP connection that will create a local server dedicated to a certain ip in which the device (Node MCU) has been connected to.
+
+We will also use MDNS i.e. Multicaste domain name system tha will give our local ip a user friendly name (default esp8266.local) from which
+we can access our server securely.
+
+
+### Components :
+
+NodeMCU, DHT11 sensor, Breadboard ,Jumper Cables
